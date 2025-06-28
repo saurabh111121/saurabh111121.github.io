2be77f80b30274b4d40b4357dd376e8eea23e57f
kalasaur <kalasaur@amazon.com>
2025-06-29 00:43:24 +0530

    basic fix

--- forms/contact.php
+++ forms/contact.php
@@ -6,8 +6,8 @@
   * For more info and help: https://bootstrapmade.com/php-email-form/
   */
 
-  // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
+  // Replace 111121saurabh@gmail.com with your real receiving email address
+  $receiving_email_address = '111121saurabh@gmail.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
